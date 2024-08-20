b3790cb22699073170d9356713fe20c22dca4d33
gennadyg <ggortalov@styleseat.com>
2024-08-19 21:33:57 -0400

    mobile view

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  viewportHeight: 932,
+  viewportWidth: 430,
   e2e: {
     // Configure your E2E tests here
     specPattern: "cypress/e2e/**/*.{cy,spec}.{js,ts}"
